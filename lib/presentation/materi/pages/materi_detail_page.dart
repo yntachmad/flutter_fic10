81beba87156254f2b6ad15cf084d23b0722bb36e
achmad <achmad.t.jayanto@gmail.com>
2024-01-01 22:22:57 +0700

    materi detail

--- lib/presentation/materi/pages/materi_detail_page.dart
+++ lib/presentation/materi/pages/materi_detail_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_fic10/core/extensions/build_context_ext.dart';
+
 // import 'package:flutter_cbt/core/extensions/build_context_ext.dart';
 
 import '../../../core/components/custom_scaffold.dart';
